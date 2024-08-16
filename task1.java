395312fba142709e24bea7d3b5a2e86907c5f32e
Gopinath J <jagadeesanhari24@gmail.com>
2024-08-16 21:28:41 +0530

    initial commit

---
+++ task1.java
@@ -0,0 +1,8 @@
+public class task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int maxRounds = 5;
+        int score = 0;
+    }
+}
